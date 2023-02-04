74fd622a23cf1166ce6526d3bd5134cf96d1f151
Oseas Neto <oseas.neto3@gmail.com>
2023-02-03 21:03:15 -0300

    update

--- src/main/java/br/com/cod3r/cm/modelo/Campo.java
+++ src/main/java/br/com/cod3r/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.cm.modelo;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +38,48 @@ public class Campo {
             return true;
         } else {
             return false;
-
         }
+    }
+
+    void alternarMarcacao(){
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir(){
+        if (!aberto && !marcado) {
+            aberto = true;
+            if (minado){
+                throw new ExplosaoException();
+            }
+
+            if(vizinhacaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean vizinhacaSegura(){
+
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+    void minar () {
+        minado = true;
+    }
+
+    public boolean isMarcado(){
+        return marcado;
+    }
+
+    public boolean isAberto(){
+        return aberto;
+    }
+    public boolean isFechado(){
+        return !isAberto();
     }
 }
--- src/test/java/br/com/cod3r/cm/modelo/CampoTest.java
+++ src/test/java/br/com/cod3r/cm/modelo/CampoTest.java
@@ -1,5 +1,6 @@
 package br.com.cod3r.cm.modelo;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -61,5 +62,69 @@ public class CampoTest {
 
         //ele espera um valor falso já que ele não é um vizinho
         Assertions.assertFalse(resultado);
+    }
+    @Test
+    void testeValorPadraoAtributoMarcado(){
+        Assertions.assertFalse(campo.isMarcado());
+    }
+    @Test
+    void testeAlternarMarcacao(){
+        campo.alternarMarcacao();
+        Assertions.assertTrue(campo.isMarcado());
+    }
+    @Test
+    void testeAlternarMarcacaoDuasChamadas(){
+        campo.alternarMarcacao();
+        campo.alternarMarcacao();
+        Assertions.assertFalse(campo.isMarcado());
+    }
+    @Test
+    void testeAbrirNaoMinadoNaoMarcado(){
+        Assertions.assertTrue(campo.abrir());
+    }
+    @Test
+    void testeAbrirNaoMinadoMarcado(){
+        campo.alternarMarcacao();
+        Assertions.assertFalse(campo.abrir());
+    }
+    @Test
+    void testeAbrirMinadoMarcado(){
+        campo.alternarMarcacao();
+        campo.minar();
+        Assertions.assertFalse(campo.abrir());
+    }
+    @Test
+    void testeAbrirMinadoNaoMarcado(){
+        campo.minar();
+        Assertions.assertThrows(ExplosaoException.class, () -> {
+            campo.abrir();
+        });
+    }
+    @Test
+    void testeAbrirComVizinhos1(){
+        Campo campo22 = new Campo(2,2);
+        Campo campo11 = new Campo(1,1);
+
+        campo22.adicionarVizinho(campo11);
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+
+        Assertions.assertTrue(campo22.isAberto() && campo11.isAberto());
+
+    }
+    @Test
+    void testeAbrirComVizinhos2(){
+        Campo campo22 = new Campo(2,2);
+        Campo campo11 = new Campo(1,1);
+        Campo campo12 = new Campo(1,2);
+        campo12.minar();
+
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+
+        Assertions.assertTrue(campo22.isAberto() && campo11.isFechado());
+
     }
 }
